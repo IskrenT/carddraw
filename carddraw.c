3e991f179d828d92739bdb81d4193da4d18c3756
IskrenT <isktark@yahoo.com>
2017-03-07 23:14:26 -0500

    Uploaded library source files for the carddraw application.

---
+++ carddraw.c
@@ -0,0 +1,86 @@
+/*  Software Name: carddraw 
+*	Use: Calculate probability of drawing subset of items(cards) from a pool.
+*   Copyright (C) 2017  Iskren Tarkalanov
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*	Contact: isktark@yahoo.com Iskren Tarkalanov
+*/
+#include "carddraw.h"
+/// carddraw function - entry point for the general computation.
+double carddraw(int total, int draw, int outs, int need, int only){
+	double result = 0.0;
+
+	// Covering all the false input cases with probability 0
+	if(total<=0 || draw<=0 || outs<=0 || need<=0 ||
+		 need > total || need > draw || need > outs 
+		)
+		return 0.0;
+	//Covering all the false input with probability 1
+	if(draw > total || outs > total){
+		if ( outs > need && only )//Covering case only==true but you draw whole deck.
+			return 0.0;
+		return 1.0;
+	}
+
+	//Computation of only=true case
+	result += (double)combination(outs,need) * (double)combination(total-outs, draw-need);
+
+	//Computation of only=false case
+	//Adding the events of drawing exactly x outs. ( x = "need+1" trough "outs" )
+	if(!only){
+		while(++need*2<=draw+outs){
+			result+=(double)combination(outs,need) * 
+							(double)combination(total-outs,draw-need);
+		}
+	}
+	//Denominator ( all combination of draws possible )
+	result/=(double)combination(total,draw);
+	return result;
+}
+/// Combinations in combinatory mathematics.
+int combination(int n, int k){
+	int result = 1;
+	int complement_k;
+		if( !k || k==n ) // one combination when k=0 or k==n
+		return 1;
+	if( !n || k>n ) //Zero combinations possible when: n=0 or k > n
+		return 0;
+
+
+	//n_C_k and n_C_(n-k) are the same, seek the least 'k'
+	complement_k = n - k;
+	if(complement_k < k ){
+		k = complement_k;
+		complement_k = n - k;
+	}
+
+	//Compute nominator
+	do // !(n-k)
+	{
+		result *= n;
+	}while(--n -complement_k); // !(n-k) (factorial)
+
+	//Compute denominator
+	do
+	{
+		result /= k;
+	}while(--k); // !k (factorial)
+
+	return result;
+}
+/// Produce the probability of the complement of the event. ( NOT EVENT )
+double complement(double probability){
+	return 1.0-probability;
+}
---
+++ carddraw.h
@@ -0,0 +1,75 @@
+/*  Software Name: carddraw 
+*	Use: Calculate probability of drawing subset of items(cards) from a pool.
+*   Copyright (C) 2017  Iskren Tarkalanov
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*	Contact: isktark@yahoo.com Iskren Tarkalanov
+*/
+// FILE: "carddraw.h" main header contains declarations of functions in "carddraw.c"
+#ifndef CARDDRAW_H
+#define CARDDRAW_H
+/// carddraw function - entry point for the general computation.
+/**
+* All parameters must be positive integers representing cardinalities of
+* the following sets and subsets: 
+* (using 'A|>B' to present B being a subset of A)
+*
+* total|>outs|>need
+* total|>draw
+* 
+* The following is true for the cardinalities:
+* |total|>=|outs|>=|need|
+* |total|>=|draw|>=|need|
+*
+* !!!CAUTION!!!
+* Although most of the reasonable exeptions are handled,
+* calling the function with arguments not following the rules above might
+* result in unexpected behaviour.
+* 
+* @param[in] total	Total number of cards or items.
+* @param[in] draw	Number of draws. ( Available or drawn, depending on use )
+* @param[in] outs	Number of useful cards or items.
+* @param[in] need	Number of needed cards or items within the useful ones.
+* @param[in] only	True  = chance of drawing only the needed ones.
+*					False = chance of drawing at least the needed ones.
+* @return probability from 0.0 to 1.0 of the following event to occur:
+*	Probabilistic event:
+	Drawing [draw] number of cards from [total] cards in which:
+	(when only = true ) there are exactly [need] number of cards from [outs]
+	(when only = false) there are at least [need] number of cards from [outs]
+*
+*   This event can be used to predict probability of possesion of certain
+*	cards, probability of drawing certain cards. Or the outcome of a random
+*	event, with some good outs. ( thinking of items instead of cards )
+*/
+double carddraw(int total, int draw, int outs, int need, int only);
+
+/// Combinations in combinatory mathematics.
+/**
+* Simple but powerful implementation of finding combinations or binomial
+* coefficients in combinatory mathematics.
+* 
+* CAUTION: NOT SAFE FOR OVERFLOW OR NEGATIVE INPUT
+* 
+* @param[in] n	Total cardinality of the set 'items'.
+* @param[in] k	Total cardinality of the subset 'picked'.
+*
+* @return	The number of possible subsets with cardinality k 
+*			within set with cardinality n.
+*/
+int combination(int n,int k);
+/// Produce the probability of the complement of the event. ( NOT EVENT )
+double complement(double probability);
+#endif
